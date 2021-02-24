12abfeedd72df26b11c35ea48b7ea5e163cb3956
neshaalaksh <scientistneshaa@gmail.com>
2021-02-24 10:53:24 +0530

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -126,7 +126,7 @@ const projects = [
   },
   {
     title: "News Letter App",
-    cardImage: "assets/images/project-page/Pet.png",
+    cardImage: "",
     description: "Built using Python.",
     tags: ["JavaScript"],
     tagimg: "https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67",
@@ -135,7 +135,16 @@ const projects = [
   },
   {
     title: "Game yet to be filled",
-    cardImage: "assets/images/project-page/Pet.png",
+    cardImage: "",
+    description: "Built using Python.",
+    tags: ["JavaScript"],
+    tagimg: "https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67",
+    Previewlink: "https://snack.expo.io/@neshaalaksh/newsletter-app",
+    Githublink: "https://github.com/neshaalaksh/Newsletterapp/",
+  },
+  {
+    title: "Game yet to be filled",
+    cardImage: "",
     description: "Built using Python.",
     tags: ["JavaScript"],
     tagimg: "https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67",
